ff4a0aa3ec8c313799b7f2b4c8755e1b6675f0f6
MrSaints <ian@fyianlai.com>
2014-05-12 06:31:32 +0100

    Fixing indentation

--- public/src/js/contact.js
+++ public/src/js/contact.js
@@ -1,31 +1,31 @@
 (function ($) {
-	/*
-	 * WARNING: Extremely messy.
-	 * @TODO: Clean up of course (use functions).
-	 */
-	$('.contact__form-input > input').on('blur keydown', function (e) {
-		if (e.type === 'keydown' && e.keyCode !== 13)
-			return;
-		e.preventDefault();
-		$(this).parent().fadeOut().prev().text( $(this).val() || $(this).parent().prev().text() ).removeClass('error');
-	});
-	$('.contact__form-confirm').click(function () {
-		$(this).parent().prev().text( $(this).prev().val() || $(this).parent().prev().text() );
-		$(this).parent().fadeOut();
-	});
+    /*
+     * WARNING: Extremely messy.
+     * @TODO: Clean up of course (use functions).
+     */
+    $('.contact__form-input > input').on('blur keydown', function (e) {
+        if (e.type === 'keydown' && e.keyCode !== 13)
+            return;
+        e.preventDefault();
+        $(this).parent().fadeOut().prev().text( $(this).val() || $(this).parent().prev().text() ).removeClass('error');
+    });
+    $('.contact__form-confirm').click(function () {
+        $(this).parent().prev().text( $(this).prev().val() || $(this).parent().prev().text() );
+        $(this).parent().fadeOut();
+    });
 
-	$('.contact__form-select > li').click(function () {
-		if ($(this).parents('.contact__form-projects').length > 0 || 
-			$(this).text() === 'join an existing project')
-			$('.contact__form-projects').fadeIn().css("display", "inline-block");
-		else
-			$('.contact__form-projects').fadeOut();
-		$(this).parent().next().val( $(this).index() );
-		$(this).parent().parent().prev().text( $(this).text() );
-		$(this).parent().parent().fadeOut();
-	});
+    $('.contact__form-select > li').click(function () {
+        if ($(this).parents('.contact__form-projects').length > 0 || 
+            $(this).text() === 'join an existing project')
+            $('.contact__form-projects').fadeIn().css("display", "inline-block");
+        else
+            $('.contact__form-projects').fadeOut();
+        $(this).parent().next().val( $(this).index() );
+        $(this).parent().parent().prev().text( $(this).text() );
+        $(this).parent().parent().fadeOut();
+    });
 
-	$('.contact__form-placeholder').click(function () {
-		$(this).next().fadeIn().find('input').focus();
-	});
+    $('.contact__form-placeholder').click(function () {
+        $(this).next().fadeIn().find('input').focus();
+    });
 })(jQuery);
--- public/src/js/enactus.js
+++ public/src/js/enactus.js
@@ -1,21 +1,21 @@
 (function ($) {
-	$(document).foundation();
+    $(document).foundation();
 
-	var $onScreenObj = $('.js-onScreen');
+    var $onScreenObj = $('.js-onScreen');
 
-	if (!Modernizr.touch)
-	    $('.footer').onScreen({
-	        doIn: function () {
-	            $onScreenObj.show();
-	        },
-	        doOut: function () {
-	            $onScreenObj.hide();
-	        },
-	        tolerance: '-50'
-	    });
-	else
-		$onScreenObj.css({
-			'bottom': '0',
-			'position': 'relative'
-		}).show();
+    if (!Modernizr.touch)
+        $('.footer').onScreen({
+            doIn: function () {
+                $onScreenObj.show();
+            },
+            doOut: function () {
+                $onScreenObj.hide();
+            },
+            tolerance: '-50'
+        });
+    else
+        $onScreenObj.css({
+            'bottom': '0',
+            'position': 'relative'
+        }).show();
 })(jQuery);
