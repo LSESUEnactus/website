c5cc31dbd46d651a77099a4f606b448940ca5432
Ian Lai <MrSaints@users.noreply.github.com>
2015-07-04 03:45:46 +0100

    Added sticky footer / auto extend content area for large screens

--- src/js/enactus.js
+++ src/js/enactus.js
@@ -18,4 +18,21 @@
             'bottom': '0',
             'position': 'relative'
         }).show();
+
+    /*
+     * Window height will not remain static (don't cache)
+     * Wrapper height is likely to remain static (cache)
+     * Calculate the difference so we only need to call height() once
+     * Listen for window resize and throttle the calls
+     */
+    var $window = $(window);
+    var $wrapperHeight = $('.wrapper').height();
+    var $container = $('.js-extend');
+    var extendContainer = function () {
+        var $difference = $window.height() - $wrapperHeight;
+        if ($difference > 0)
+            $container.css('height', $difference);
+    }
+    extendContainer();
+    $window.on('resize', Foundation.utils.throttle(extendContainer, 300));
 })(jQuery);
